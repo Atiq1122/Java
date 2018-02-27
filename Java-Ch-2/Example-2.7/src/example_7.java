36db4e1316cdf67d13952b8b8528621c7afdcb21
ItsSample <Shujadani72@gmail.com>
2018-02-27 05:32:26 -0800

    Example 2.7 edit

---
+++ Java-Ch-2/Example-2.7/src/example_7.java
@@ -0,0 +1,22 @@
+
+	import java.util.Scanner;
+
+	public class example_7 {
+
+		public static void main(String[] args) {
+			Scanner input = new Scanner(System.in);
+			int x;
+			int y;
+			int z;
+			int result;
+			System.out.print("Enter first integer: ");
+			x = input.nextInt();
+			System.out.print("Enter second integer: ");
+			y = input.nextInt();
+			System.out.print("Enter third integer: ");
+			z = input.nextInt();
+			result = x * y * z;
+			input.close();
+			System.out.printf("Product is %d\n", result);
+		}
+	}
