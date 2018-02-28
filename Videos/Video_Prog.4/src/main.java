fbe98872b2f43f690b47b5169298a63c9373f5fb
RanaRaaj <36886757+RanaRaaj@users.noreply.github.com>
2018-02-28 14:28:25 +0500

    Video Program 4 edit

---
+++ Videos/Video_Prog.4/src/main.java
@@ -0,0 +1,26 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		double [] values = new double[10];
+		values[0] = 999;
+		values[2] = 344.4;
+		values[9] = 2342.00;
+		
+		System.out.println(values[9]);
+		
+		String [] words = new String[5];
+		words[3] = "This";
+		words[4] = "World";
+		words[2] = "Is";
+		
+		System.out.println(words[2]);
+		
+		words = new String[10];
+		System.out.println(words[9]);
+		
+
+	}
+
+}
