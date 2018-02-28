9cea750120bdb5b4f1546a9c468aa0255f3a0fd8
ItsSample <Shujadani72@gmail.com>
2018-02-28 01:15:23 -0800

    Exercise-2.26 edit

---
+++ Java-Ch-2/Exercise-2.26/src/exert.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class exert {
+public static void main(String[] args) {
+	Scanner input=new Scanner(System.in);
+	int num1;
+	int num2;
+	System.out.println("Enter 1st num");
+	num1 = input.nextInt();
+	System.out.println("enter 2nd num");
+	num2= input.nextInt();
+	if(num1%num2 == 0)
+		System.out.println(num1+" is multiple of" +num2+ "number");
+	else
+		System.out.println("not multiple");
+	input.close();
+}
+}
+
