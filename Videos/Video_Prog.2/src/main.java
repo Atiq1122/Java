6e56fc36f37c245d86e9d4e678fc0bf60db48145
RanaRaaj <36886757+RanaRaaj@users.noreply.github.com>
2018-02-28 14:22:49 +0500

    Videos Programs 2 edit

---
+++ Videos/Video_Prog.2/src/main.java
@@ -0,0 +1,16 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		int x = 34;
+		System.out.println(x);
+		
+		x = 234;
+		System.out.println(x * 4);
+		System.out.println(x);
+		String words = "this is a sentence";
+		System.out.println(words + "these are some more words");
+
+	}
+
+}
