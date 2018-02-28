492cb91270dce8b4fff69d17cd39333569a5fe88
ItsSample <Shujadani72@gmail.com>
2018-02-27 23:58:31 -0800

    Exercise-2.17 edit

---
+++ Java-Ch-2/Execrise-2.17/src/exercise17.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class exercise17 {
+public static void main(String[] args) {
+	Scanner input=new Scanner(System.in);
+	int num1,num2,num3,sum,avg,prod;
+	System.out.println("Enter first integer");
+	num1=input.nextInt();
+	System.out.println("Enter second integer");
+	num2=input.nextInt();
+	System.out.println("Enter third integer");
+	num3=input.nextInt();
+	sum=num1+num2+num3;
+	prod=num1*num2*num3;
+	avg=(prod)/3;
+	System.out.println("Sum is:"+sum);
+	System.out.println("Product is:"+prod);
+	System.out.println("Average is:"+avg);
+	if(num1<num2&&num1<num3)
+		System.out.println(num1+" is minimum integer");
+	else if(num2<num1&&num2<num3)
+		System.out.println(num2+" is minimum integer");
+	else if(num3<num1&&num3<num2)
+		System.out.println(num3+" is minimum integer");
+	
+	if(num1>num2&&num1>num3)
+		System.out.println(num1+" is maximum integer");
+	else if(num2>num1&&num2>num3)
+		System.out.println(num2+" is maximum integer");
+	else if(num3>num1&&num3>num2)
+		System.out.println(num3+" is maximum integer");
+	input.close();
+}
+}
