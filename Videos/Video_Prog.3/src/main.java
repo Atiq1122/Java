771ce6cbe2e3f85bc057c17af0fdd8bea8b79ba7
RanaRaaj <36886757+RanaRaaj@users.noreply.github.com>
2018-02-28 14:26:57 +0500

    Video Program 3 edit

---
+++ Videos/Video_Prog.3/src/main.java
@@ -0,0 +1,20 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		int var = 1000000000;  //2^31
+		long bigNumber = 10000000000000L;
+		
+		short smallNumber = -32768; // -32768 To 32767
+		byte verySmall = -128; // -128 To 127
+		
+		double floatNumber = 123.234;
+		
+		char oneNumber = 'h';
+		
+		boolean decision = true; // it's for true or false
+
+	}
+
+}
