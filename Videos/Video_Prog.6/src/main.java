5995777148a4aedebc36d42c23d058cdd00738e8
RanaRaaj <36886757+RanaRaaj@users.noreply.github.com>
2018-03-01 23:42:12 +0500

    Video Program 6 edit

---
+++ Videos/Video_Prog.6/src/main.java
@@ -0,0 +1,13 @@
+
+public class main {
+
+	public static void main(String[] args) {
+//		System.out.println("Welcome here:");
+		System.out.println(main2.printSomeJunk(" Hello there"));
+//		main2.sum2Numbers(12,34);
+//		int myvar = main2.add10(99)+ 1000-50;
+//		System.out.println(myvar);
+
+	}
+	
+}
