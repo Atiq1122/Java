abfb81cda7d4cb7fab781d6b7577a7ef4214c2a2
tehreemazhar <36886700+tehreemazhar@users.noreply.github.com>
2018-03-04 18:18:51 +0500

    Example#1 edit

---
+++ Java-Ch-3/Example#1/src/gradebook.java
@@ -0,0 +1,8 @@
+
+public class gradebook {
+	public void displaymessage() {
+		System.out.println("welcome to the grade book!");
+		
+	}
+
+}
---
+++ Java-Ch-3/Example#1/src/gradebooktest.java
@@ -0,0 +1,8 @@
+
+public class gradebooktest {
+	public static void main(String[] args) {
+		gradebook mygradebook=new gradebook();
+		mygradebook.displaymessage();
+	}
+
+}
