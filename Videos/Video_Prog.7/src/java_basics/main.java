ed5cde0660bc43ad0e7cffa8254112c0a030a2eb
RanaRaaj <36886757+RanaRaaj@users.noreply.github.com>
2018-03-05 12:54:30 +0500

    Video Program 7 edit

---
+++ Videos/Video_Prog.7/src/java_basics/main.java
@@ -0,0 +1,19 @@
+package java_basics;
+
+import someotherpackage.ExampleClass;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		main2.printSomeJunk(34);
+//		System.out.println("Welcome here:");
+		System.out.println(main2.printSomeJunk(" Hello there"));
+//		main2.sum2Numbers(12,34);
+		int myVar = main2.add10(99)+ 1000-50;
+//		System.out.println(myVar);
+		ExampleClass.dosomething();
+
+	}
+	
+}
---
+++ Videos/Video_Prog.7/src/someotherpackage/ExampleClass.java
@@ -0,0 +1,10 @@
+package someotherpackage;
+
+import java_basics.main2;
+
+public class ExampleClass {
+	public static void dosomething() {
+		main2.printSomeJunk(34);
+	}
+
+}
