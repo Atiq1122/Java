21fd2b12d82100a61bb4dbfbb7d3ac913e67c975
RanaRaaj <36886757+RanaRaaj@users.noreply.github.com>
2018-03-14 01:16:15 +0500

    Example 5.7

---
+++ Java-Ch-5/Example7ch5/src/breakTest.java
@@ -0,0 +1,15 @@
+
+public class breakTest {
+
+	public static void main(String[] args) {
+		int count;
+		for	( count = 1; count <= 10; count++ ) 
+		{
+		if ( count == 7 )
+			break;
+						System.out.printf( "%d ", count );
+		}
+				System.out.printf( "\nBroke out of loop at count = %d\n", count );
+	}
+
+}
