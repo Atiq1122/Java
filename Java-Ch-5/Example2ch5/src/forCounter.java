800e80041129d046019688ff24884d87d56c7862
RanaRaaj <36886757+RanaRaaj@users.noreply.github.com>
2018-03-14 01:15:13 +0500

    Example 5.2

---
+++ Java-Ch-5/Example2ch5/src/forCounter.java
@@ -0,0 +1,12 @@
+
+public class forCounter {
+
+	public static void main(String[] args) {
+		
+		for ( int counter = 1; counter <= 10; counter++ )
+			System.out.print( " " + counter );
+		System.out.println();
+
+	}
+
+}
