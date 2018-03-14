64b23c4731c64227a88a5a9af4bc8a803792ec3b
umer207 <36888810+umer207@users.noreply.github.com>
2018-03-14 10:43:07 +0500

    example#5.3 chp#5

---
+++ Java-Ch-5/chap#5 example_5.3/src/sum.java
@@ -0,0 +1,13 @@
+
+public class sum {
+
+	public static void main(String[] args) {
+		int total = 0;
+		for ( int num = 2; num <= 20; num+=2 )
+			total += num;
+		System.out.printf( "Sum is %d\n", total );
+		
+
+	}
+
+}
